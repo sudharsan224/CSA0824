c34afadddbc3720e962adaac178b3d4f412b2ee3
sudharsan224 <192524063.simats@saveetha.com>
2025-11-13 14:24:27 +0530

    Fix formatting and spacing in swap script

--- swap using temperary variable.py
+++ swap using temperary variable.py
@@ -1,9 +1,9 @@
-a = 5
-b = 10
+a=5
+b=10
 print("Before swapping:")
-print("a =",a,"b =",b)
-temp =a
-a =b
-b =temp
+print("a=",a,"b=",b)
+temp=a
+a=b
+b=temp
 print("After swapping:")
-print("a =",a,"b =",b)
+print("a=",a,"b=",b)
