07d520a3fa75fc28cd2adcd113668b2fc549178c
sudharsan224 <192524063.simats@saveetha.com>
2025-11-13 14:15:25 +0530

    Add files via upload

---
+++ To convert the temperature celcius to farheint.py
@@ -0,0 +1,4 @@
+c=int(input("enter a temperature:"))
+f=(c*9/5)+32
+print("enter a celesius to fahrenheit:",f)
+
---
+++ To find the simple interest.py
@@ -0,0 +1,5 @@
+p=int(input("enter a number:"))
+q=int(input("enter a number:"))
+r=int(input("enter a number:"))
+n=p*q*r/100
+print("enter a simple intrest:",n)
